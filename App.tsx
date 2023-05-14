e970f9a720f3c85f501224351ca90fc8f426925f
Edith-Samuel Sbenghe <72344412+hctensa@users.noreply.github.com>
2023-05-14 19:32:12 +0100

    Created Basic Navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
+// IMPORTING ALL LIBRARIES
 import * as React from 'react';
+import * as Native from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import * as RN from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//IMPORTING NERDIO ASSETS
+import Style from './assets/style'
+
 
 function App(): JSX.Element {
+  // === SCREENS
+  const HomeScreen = () => {
+    return (
+      <Native.View>
+        <Native.Text style={Style.Heading}>This is the home screen</Native.Text>
+      </Native.View>
+    )
+  }
+  const GameScreen = () => {
+    return (
+      <Native.View>
+        <Native.Text>This is the game screen</Native.Text>
+      </Native.View>
+    )
+  }
+
+  // === GAME LOGIC
+
+  // === NAVIGATION
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-      <RN.Text>Hello World!</RN.Text>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Game" component={GameScreen}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ assets/style.tsx
@@ -0,0 +1,9 @@
+import * as Native from 'react-native';
+
+const Style = Native.StyleSheet.create({
+    Heading : {
+        fontSize: 30
+    }
+})
+
+export default Style;
