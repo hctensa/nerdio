a888a8c3c08cfc01955541ad0280462219170daa
Edith-Samuel Sbenghe <72344412+hctensa@users.noreply.github.com>
2023-05-15 01:37:55 +0100

    Working app without timer

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ const QuestionBackground = require('./assets/images/question_background.png');
 
 function App(): JSX.Element {
   // === GAME LOGIC
-  const [activeLevelScore, setActiveLevelScore] = React.useState(1);
+  const [activeScore, setActiveScore] = React.useState(1);
   const [Difficulty, setDifficulty] = React.useState('Easy');
   const [number1, setNumber1] = React.useState(1);
   const [number2, setNumber2] = React.useState(1);
@@ -22,36 +22,104 @@ function App(): JSX.Element {
   const [correctAnswer, setCorrectAnswer] = React.useState(0);
 
   React.useEffect(()=>{
-    if (activeLevelScore >= 11){
-      setActiveLevelScore(1);
+    if (activeScore >= 11){
+      setActiveScore(1);
     }
-  }, [activeLevelScore])
+  }, [activeScore])
 
-  const SwapDifficulty = (level:string) => {
-    setActiveLevelScore(1)
-    switch (Difficulty) {
-      case 'easy':
-        setDifficulty('easy');
-        break;
-      case 'medium':
-        setDifficulty('medium');
-        break;
-      case 'hard':
-        setDifficulty('hard');
-        break;
-      default:
-        break;
+  const SwapDifficulty = (difficulty_internal:string) => {
+    setActiveScore(1)
+    switch (difficulty_internal) {
+      case 'easy': setDifficulty('easy'); break;
+      case 'medium': setDifficulty('medium'); break;
+      case 'hard': setDifficulty('hard'); break;
+      default: break;
     }
   }
 
   const ValidateAnswer = () => {
     if(userAnswer === correctAnswer){
-      setActiveLevelScore(activeLevelScore+1)
+      setActiveScore(activeScore+1)
+    }
+    GenerateNewQuestion()
+  }
+
+  const GenerateNewQuestion = () => {
+    switch (GenerateRandomNumber1to4Inclusive()) {
+      case 1: /* Addition */ GenerateAdditionQuestion(); break;
+      case 2: /* Multiplication */ GenerateMultiplicationQuestion(); break;
+      case 3: /* Subtraction */ GenerateSubtractionQuestion(); break;
+      case 4: /* Division */ GenerateDivisionQuestion(); break;
+      default: break;
     }
   }
 
-  const GenerateNewQuestion = (number1:number, number2:number, mathSymbol:string) => {
-    
+  const GenerateAdditionQuestion = () => {
+    const localNumber1:number = GenerateRandomNumber1to12Inclusive();
+    const localNumber2:number = GenerateRandomNumber1to12Inclusive();
+    const localCorrectAnswer:number = localNumber1+localNumber2;
+    setNumber1(localNumber1);
+    setNumber2(localNumber2);
+    setMathSymbol("+");
+    setCorrectAnswer(localCorrectAnswer);
+  }
+
+  const GenerateMultiplicationQuestion = () => {
+    const localNumber1:number = GenerateRandomNumber1to12Inclusive();
+    const localNumber2:number = GenerateRandomNumber1to12Inclusive();
+    const localCorrectAnswer:number = localNumber1*localNumber2;
+    setNumber1(localNumber1);
+    setNumber2(localNumber2);
+    setMathSymbol("x");
+    setCorrectAnswer(localCorrectAnswer);
+  }
+
+  const GenerateSubtractionQuestion = () => {
+    const localNumber1:number = GenerateRandomNumber7to12Inclusive();
+    const localNumber2:number = GenerateRandomNumber1to6Inclusive();
+    const localCorrectAnswer:number = localNumber1-localNumber2;
+    setNumber1(localNumber1);
+    setNumber2(localNumber2);
+    setMathSymbol("-");
+    setCorrectAnswer(localCorrectAnswer);
+  }
+
+  const GenerateDivisionQuestion = () => {
+    let localNumber1 = Math.floor(Math.random() * 12) + 1;
+    let localNumber2 = Math.ceil(Math.random() * (12 / localNumber1)) * localNumber1;
+    const localCorrectAnswer:number = localNumber1/localNumber2;
+    setNumber1(localNumber1);
+    setNumber2(localNumber2);
+    setMathSymbol("÷");
+    setCorrectAnswer(localCorrectAnswer);
+  }
+
+  const GenerateRandomNumber1to4Inclusive = () => {
+    const randomNumber = Math.random();
+    const scaledNumber = randomNumber * 4;
+    const finalNumber = Math.floor(scaledNumber) + 1;
+    return finalNumber;
+  }
+
+  const GenerateRandomNumber1to12Inclusive = () => {
+    const randomNumber = Math.random();
+    const scaledNumber = randomNumber * 12;
+    const finalNumber = Math.floor(scaledNumber) + 1;
+    return finalNumber;
+  }
+  
+  const GenerateRandomNumber7to12Inclusive = () => {
+    const randomNumber = Math.random();
+    const scaledNumber = randomNumber * 6;
+    const finalNumber = Math.floor(scaledNumber) + 7;
+    return finalNumber;
+  }
+
+  const GenerateRandomNumber1to6Inclusive = () => {
+    const randomNumber = Math.random();
+    const scaledNumber = randomNumber * 6;
+    const finalNumber = Math.floor(scaledNumber) + 1;
+    return finalNumber;
   }
 
   // === SCREENS
@@ -80,17 +148,16 @@ function App(): JSX.Element {
         <Native.ImageBackground source={AppHomeBackground} style={Style.BackgroundImage}>
           <Native.View style={Style.TopView}>
             <Native.View style={Style.MiddleView}>
-              <Native.Text style={Style.ScoreCounter}>Question {activeLevelScore}/10</Native.Text>
+              <Native.Text style={Style.ScoreCounter}>Question {activeScore}/10</Native.Text>
               <Native.Text style={Style.Countdown}>Time Left: 0 Seconds</Native.Text>
             </Native.View>
           </Native.View>
           <Native.View style={Style.MiddleView}>
             <Native.ImageBackground source={QuestionBackground} style={Style.QuestionBackgroundImage}>
               <Native.Text style={Style.Question}>What is {number1} {mathSymbol} {number2}?</Native.Text>
-              <Native.Text>{userAnswer}</Native.Text>
               <Native.TextInput style={Style.AnswerBox} placeholder='Enter answer here...' keyboardType='numeric' value={String(userAnswer)} textAlign='center' onChangeText={(inputText) => setUserAnswer(Number(inputText))}/>
-              <Native.TouchableOpacity style={Style.HandInButton}>
-                <Native.Text style={Style.HandInButtonText} onPress={ValidateAnswer}>Hand In</Native.Text>
+              <Native.TouchableOpacity style={Style.HandInButton} onPress={ValidateAnswer}>
+                <Native.Text style={Style.HandInButtonText}>Hand In</Native.Text>
               </Native.TouchableOpacity>
             </Native.ImageBackground>
           </Native.View>
